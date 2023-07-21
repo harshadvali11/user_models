94e8531ec57fd6f96dc08155c37b96ff004c3eea
harshadvali11 <harshadvali1431@gmail.com>
2023-07-21 14:10:55 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
+from django.urls import reverse
 # Create your views here.
 from django.core.mail import send_mail
 from app.forms import *
+from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.decorators import login_required
+
+
+
 def registration(request):
     usfo=UserForm()
     pfo=ProfileForm()
@@ -36,6 +42,40 @@ def registration(request):
 
 
     return render(request,'registration.html',d)
+
+def home(request):
+    if request.session.get('username'):
+        username=request.session.get('username')
+        d={'username':username}
+        return render(request,'home.html',d)
+
+    return render(request,'home.html')
+
+
+def user_login(request):
+    if request.method=='POST':
+        username=request.POST['username']
+        password=request.POST['password']
+        AUO=authenticate(username=username,password=password)
+        if AUO:
+            if AUO.is_active:
+                login(request,AUO)
+                request.session['username']=username
+                return HttpResponseRedirect(reverse('home'))
+            else:
+                return HttpResponse('Not a Active User')
+        else:
+            return HttpResponse('Invalid Details')
+    return render(request,'user_login.html')
+
+
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('home'))
+
+
 
 
 
--- project23/urls.py
+++ project23/urls.py
@@ -22,6 +22,9 @@ from app.views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('registration/',registration,name='registration'),
+    path('home/',home,name='home'),
+    path('user_login/',user_login,name='user_login'),
+    path('user_logout/',user_logout,name='user_logout'),
 
 
 
